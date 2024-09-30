c6aa724752347ee7159b51db1c6fc6301a37610a
Michael Howitz <icemac@gmx.net>
2024-09-30 09:23:14 +0200

    Preparing release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 *******
 
-1.1 (unreleased)
+1.1 (2024-09-30)
 ================
 
 - Add support for Python 3.12, 3.13.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.1.dev0'
+version = '1.1'
 
 with open("README.rst") as f:
     README = f.read()
